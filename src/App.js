3108bc6e84ae8b5d330d25b32675359a8f1097db
sgershuny <72019925+sgershuny@users.noreply.github.com>
2020-12-09 12:39:35 +0000

    This is practice of starting a React App from scratch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const Avenger = props => {
+  return(
+    <div>
+      <h1>{props.avenger}</h1>
     </div>
-  );
+  )
+}
+
+class App extends React.Component {
+
+  constructor(){
+    super();
+    this.state = {
+      Avenger: ["Iron Man","Spider-Man","Hulk","Steve Bannon"]
+    }
+  }
+  
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          {this.state.Avenger.map(hero => {
+            console.log(hero)
+            return <Avenger avenger = {hero}/>
+          })}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
